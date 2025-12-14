dca292dbf8edb01cfad189fdf57c41d247099075
Alexander <alexander30409@gmail.com>
2025-12-14 15:34:38 -0400

    require

--- modelos/modulo.py
+++ modelos/modulo.py
@@ -59,6 +59,7 @@ class Student(BaseModel):
     telefono: Mapped[str]=mapped_column(String(11))#unique
     direccion: Mapped[str] = mapped_column(LONGTEXT)
     #fecha de nacimiento
+    #genero
     
     def __repr__(self) -> str:
         return f"Student {self.primer_nombre} {self.primer_apellido} CIV:{self.cedula}"
